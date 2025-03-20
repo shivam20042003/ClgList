c25b35db9bac7d39a2b3f3f07eb9cde6b194f497
Shivam Singh <shivamsingh@Shivams-MacBook-Air.local>
2025-03-20 20:29:48 +0530

    added filters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,12 @@ export default function Home() {
   }))
   const [i,setI] = useState(0);
   const [rank,setRank] = useState(0);
+  const [catagory,setCatagory] = useState("OPEN");
   const [displaySpace,setDisplaySpace] = useState(200);
+  const catagoryList = ["EWS","EWS (PwD)","OBC-NCL","OBC-NCL (PwD)","OPEN","OPEN (PwD)","SC","SC (PwD)","ST","ST (PwD)"]
+  const [gender,setGender] = useState("Gender-Neutral");
+  const [quota,setQuota] = useState("AI");
+  const quotaList = ["AI","GO","HS","JK","LA","OS"];
   const initialSortedList = sortedData.filter((y)=>{
     const z = sortedData.indexOf(y)<displaySpace&&sortedData.indexOf(y)>=0
     return z
@@ -34,6 +39,12 @@ export default function Home() {
     })
     setSortedData(tempSortedData);
   }
+  useEffect(()=>{
+    const tempSortedData = data.filter((y)=>{
+      return y.SeatType == catagory &&  y.Gender == gender && y.Quota == quota
+    })
+    setSortedData(tempSortedData);
+  },[catagory,gender,quota]);
   
   return (
     <>
@@ -45,6 +56,24 @@ export default function Home() {
      <button onClick={()=>{if(i-displaySpace>=0)setI(i-displaySpace)}}>-</button>
      <button onClick={()=>{if(sortedData.length>i+displaySpace) setI(i+displaySpace)}}>+</button>
      <input placeholder="JEE Mains Rank" type="number" onChange={(e)=>{setRank(parseInt(e.target.value))}} /> <button onClick={()=>{rankChange()}}>Check with Rank</button>
+     <select defaultValue="OPEN" onChange={(e)=>{setCatagory(e.target.value)}} name="catagory">
+      {catagoryList.map((val)=>{
+        return (
+          <option key={val} value={val}>{val}</option>
+        )
+      })}
+    </select>
+    <select name="gender" defaultValue="Gender-Neutral" onChange={(e)=>{setGender(e.target.value)}}>
+      <option value="Gender-Neutral">Gender-Neutral</option>
+      <option value="Female-only (including Supernumerary)">Female-only including Supernumerary</option>
+    </select>
+    <select name="Quota" defaultValue="AI" onChange={(e)=>{setQuota(e.target.value)}}>
+      {quotaList.map((val)=>{
+        return (
+          <option key={val} value="val">{val}</option>
+        )
+      })}
+    </select>
       <table>
         <thead>
         <tr>
